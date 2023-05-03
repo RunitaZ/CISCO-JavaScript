b411f6033db63e7370e0ad5200c3d70264cb3ba7
RunitaZ <zotar.rut.05tae@gmail.com>
2023-05-02 22:10:40 -0400

    asincronia ejemplo

---
+++ JSON.js
@@ -0,0 +1,14 @@
+const usuarios = `{
+    "usuarios": [
+        {
+            "primerNombre": "Maria1",
+            "segundoNombre": "Paola1",
+        },
+        {
+            "primerNombre": "Maria2",
+            "segundoNombre": "Paola2",
+        }
+    ]
+}`
+
+
